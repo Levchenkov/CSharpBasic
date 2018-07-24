2bf2d3608e2cce3339853ed14f448c8881061c34
Eugene Levchenkov <e.levchenkov@itransition.com>
2018-07-24 23:04:13 +0300

    Add collections lesson.

---
+++ src/Collections/Collections/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace Collections
+{
+    /// <summary>
+    /// Links: 
+    /// 1) About collections on russian: https://blog.rc21net.ru/%D0%BA%D0%BE%D0%BB%D0%BB%D0%B5%D0%BA%D1%86%D0%B8%D0%B8-%D0%B2-c-sharp/
+    /// 2) About collections on MSDN: https://msdn.microsoft.com/en-us/library/system.collections(v=vs.110).aspx
+    /// 3) About collections on Docs (but not in this case): https://docs.microsoft.com/en-us/dotnet/standard/collections/commonly-used-collection-types
+    /// 4) Source code of BCL: https://referencesource.microsoft.com/
+    /// 5) Source code for lessons: https://github.com/Levchenkov/CSharpBasic
+    /// *) Hidden features of C#: https://stackoverflow.com/questions/9033/hidden-features-of-c
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////interfaces:
+
+            //IEnumerator
+            //IEnumerable
+            //IEnumerator<T>
+            //IEnumerable<T>
+
+            //ICollection
+            //ICollection<T>
+
+            //IList
+            //IList<T>
+            //IReadOnlyList<T>
+
+            //Array
+
+            ////classes
+
+            //ArrayList
+            //List<T>
+
+            //LinkedList<T>
+
+            //Queue
+            //Queue<T>
+
+            //Stack
+            //Stack<T>
+
+            //BitArray
+
+            //HashSet<T>
+            //SortedSet<T>
+
+            ////dictionaries
+
+            //IDictionary
+            //IDictionary<TKey, TValue>
+            //IReadOnlyDictionary<TKey, TValue>
+
+            //Hashtable
+            //ListDictionary
+            //HybridDictionary
+            //OrderedDictionary
+            //SortedDictionary <K,V>
+            //SortedList <K,V>
+            //SortedList
+
+            ////configurable collections
+
+            //Collection<T>
+            //CollectionBase
+
+            //KeyedCollection<TKey,TItem> 
+            //DictionaryBase
+
+            //ReadOnlyCollection<T>
+        }
+    }
+}
